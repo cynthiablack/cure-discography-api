9b4d8560fdeb81a8e0e04c3dffb66b36c6a0da46
Cynthia Black <cindycblack@gmail.com>
2022-06-14 18:08:31 -0800

    Initial upload

---
+++ README.md
@@ -0,0 +1,35 @@
+# My Awesome Project
+The Cure's Discogrphy API is a searchable public API containing the catalog of The Cure's live and studio albums along with compilations, and extended play releases.
+
+**Link to project:** http://recruiters-love-seeing-live-demos.com/
+
+![alt tag](http://placecorgi.com/1200/650)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, Node, Express
+
+Here's where you can go to town on how you actually built this thing. Write as much as you can here, it's totally fine if it's not too much just make sure you write *something*. If you don't have too much experience on your resume working on the front end that's totally fine. This is where you can really show off your passion and make up for that ten fold.
+
+
+
+This is a public API containing the catalog of 
+        The catalog includes album title, release year, label, and album category (studio, live, compilation, or extended play).
+        Users can access the API directly or by using the search field in the Search the Catalog section of this page.
+
+## Optimizations
+*(optional)*
+
+You don't have to include this section but interviewers *love* that you can not only deliver a final product that looks great but also functions efficiently. Did you write something then refactor it later and the result was 5x faster than the original implementation? Did you cache your assets? Things that you write in this section are **GREAT** to bring up in interviews and you can use this section as reference when studying for technical interviews!
+
+## Planned Enhancements
+
+- Add singles
+- Add songlist catalog
+- Add search fields for: release year, covers, song length, songwriter, country of release, chart position
+
+## Lessons Learned:
+
+No matter what your experience level, being an engineer means continuously learning. Every time you build something you always have those *whoa this is awesome* or *fuck yeah I did it!* moments. This is where you should share those moments! Recruiters and interviewers love to see that you're self-aware and passionate about growing.
+
+## License
---
+++ server.js
@@ -0,0 +1,249 @@
+const express = require('express')
+const app = express()
+const PORT = 1978
+
+const discography = {
+//studio albums
+    'three imaginary boys':{
+        'released': '1979',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'seventeen seconds':{
+        'released': '1980',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'faith':{
+        'released': '1981',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'pornography':{
+        'released': '1982',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'the top':{
+        'released': '1984',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'the head on the door':{
+        'released': '1985',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'kiss me, kiss me, kiss me':{
+        'released': '1987',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'disintegration':{
+        'released': '1989',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'wish':{
+        'released': '1992',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'wild mood swings':{
+        'released': '1996',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'bloodflowers':{
+        'released': '2000',
+        'album category': 'studio',
+        'label': 'Fiction'
+    },
+
+    'the cure':{
+        'released': '2004',
+        'album category': 'studio',
+        'label': 'Geffen'
+    },
+
+    '4:13 dream':{
+        'released': '2008',
+        'album category': 'studio',
+        'label': 'Geffen'
+    },
+
+//live albums
+    'the cure live':{
+        'released': '1984',
+        'album category': 'live',
+        'label': 'Fiction'
+    },
+
+    'entreat':{
+        'released': '1991',
+        'album category': 'live',
+        'label': 'Fiction'
+    },
+
+    'show':{
+        'released': '1993',
+        'album category': 'live',
+        'label': 'Fiction'
+    },
+
+    'paris':{
+        'released': '1993',
+        'album category': 'live',
+        'label': 'Fiction'
+    },
+
+    'bestival live 2011':{
+        'released': '2011',
+        'album category': 'live',
+        'label': 'Sunday Best'
+    },
+
+    '40 live':{
+        'released': '2019',
+        'album category': 'live',
+        'label': 'Universal Music Group'
+    },
+
+//compilations
+    "boys don't cry":{
+        'released': '1980',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'happily ever after':{
+        'released': '1981',
+        'album category': 'compilation',
+        'label': 'A&M'
+    },
+
+    'japanese whispers':{
+        'released': '1983',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'standing on a beach':{
+        'released': '1986',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'mixed up':{
+        'released': '1990',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'galore':{
+        'released': '1997',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'greatest hits':{
+        'released': '2001',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'join the dots':{
+        'released': '2004',
+        'album category': 'compilation',
+        'label': 'Fiction'
+    },
+
+    'torn down':{
+        'released': '2018',
+        'album category': 'compilation',
+        'label': 'data not available'
+    },
+
+//extended play
+    'a single':{
+        'released': '1982',
+        'album category': 'extended play',
+        'label': 'Fiction'
+    },
+
+    'the walk':{
+        'released': '1983',
+        'album category': 'extended play',
+        'label': 'Sire'
+    },
+
+    'half an octopuss & quadpus':{
+        'released': '1986',
+        'album category': 'extended play',
+        'label': 'Fiction'
+    },
+
+    'the peel sessions':{
+        'released': '1988',
+        'album category': 'extended play',
+        'label': 'Strange Fruit'
+    },
+
+    'integration':{
+        'released': '1990',
+        'album category': 'extended play',
+        'label': 'Elektra'
+    },
+
+    'lost wishes':{
+        'released': '1993',
+        'album category': 'extended play',
+        'label': 'Fiction'
+    },
+
+    'five swing live':{
+        'released': '1987',
+        'album category': 'extended play',
+        'label': 'Fiction'
+    },
+
+    'festival 2005':{
+        'released': '2006',
+        'album category': 'extended play',
+        'label': 'Geffen'
+    },
+
+    'hypnogogic states':{
+        'released': '2008',
+        'album category': 'extended play',
+        'label': 'Geffen'
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:album', (request, response)=>{
+    const album = request.params.album.toLowerCase()
+    if (discography[album]){
+        response.json(discography[album])
+    }
+    else {
+        response.json(discography['Album not found'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is running on port ${PORT} and everything is cold now.`)
+})
