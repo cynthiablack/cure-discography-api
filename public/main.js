53a998f5d797aa7c5c65eb217d3f069a07e5f212
Cynthia Black <cindycblack@gmail.com>
2022-06-19 17:19:08 -0800

    JS not working

--- public/main.js
+++ public/main.js
@@ -7,10 +7,27 @@ async function apiRequest(){
     try{
         const response = await fetch('https://cure-discography.herokuapp.com/api/${album}')
         const data = await response.json()
-
         console.log(data)
-        document.querySelector('#label').innerText = `Label: ${data.label}`
-    }catch(error){
+
+        let albumTitle = document.querySelector('#album').value
+        console.log(albumTitle)
+
+        let category = document.querySelector('#category').value
+        console.log(category)
+
+        let year = document.querySelector('#year').value
+        console.log(year)
+
+        let label = document.querySelector('#label').value
+        console.log(label)
+
+        //let label = document.getElementById('label').innerText;
+        //label = document.getElementById('label') = `Label: ${label}`
+        //document.getElementById('label');
+
+        //.innerText = `Label: ${data.label}`
+    }
+    catch(error){
         console.log(error)
     }
     }
